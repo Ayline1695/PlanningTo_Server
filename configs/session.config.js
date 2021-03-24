2f97bc834faffb2683210d9e0ccea657b5c2ec9e
ayline <ayline1695@hotmail.com>
2021-03-24 20:59:00 +0100

    config

--- configs/session.config.js
+++ configs/session.config.js
@@ -2,15 +2,21 @@ require("dotenv").config();
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 
-const { SESSION_SECRET, MONGODB_URI } = process.env;
+const { SESSION_SECRET, MONGODB_URI, NODE_ENV } = process.env;
+
+const isProduction = NODE_ENV === "production";
 
 module.exports = (app) => {
+  if (isProduction) {
+    app.set("trust proxy", 1);
+  }
+
   app.use(
     session({
       secret: SESSION_SECRET,
       resave: false,
       saveUninitialized: true,
-      cookie: { maxAge: 6000000 },
+      cookie: { maxAge: 6000000, secure: isProduction ? true : false },
       store: MongoStore.create({
         mongoUrl: MONGODB_URI,
         ttl: 60 * 60 * 24,
